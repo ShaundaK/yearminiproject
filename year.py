57ff68924d266fed1d392570dc88021a8f0b8d05
ShaundaK <sdking1913@gmail.com>
2020-12-08 23:18:32 -0600

    initial commit

---
+++ year.py
@@ -0,0 +1,11 @@
+#create a program that asks the user to enter their name and age. Print out message addressed to them that tells them the year that they will turn 100 years old.
+#Then ask user for another number and print out that many copies of the previous message
+name = input("What is your name?  ")
+age = input("What is your age?  ")
+nage = int(age)
+year = (100 - nage) + 2020
+print("Hello, {}! You will be 100 in year {}!".format(name.capitalize(), year))
+number = int(input("Please enter a number.  "))
+a = ("Hello, {}! You will be 100 in year {}!".format(name.capitalize(), year))
+for line in range(number):
+    print(a)
