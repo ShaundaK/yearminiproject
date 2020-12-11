d6fa5ffde2dc7e7435d168852b60f8fc7ba2438f
ShaundaK <sdking1913@gmail.com>
2020-12-10 18:44:36 -0600

    return statement format correct

--- year.py
+++ year.py
@@ -1,7 +1,7 @@
 #create a program that asks the user to enter their name and age. Print out message addressed to them that tells them the year that they will turn 100 years old.
 #Then ask user for another number and print out that many copies of the previous message
 def year_til(name, year):
-    return "Hello" + name + "! You will be 100 in year" + str(year) + "!".format(name.capitalize())
+    return "Hello " + name.capitalize() + "! You will be 100 in year " + str(year) + " !"
 name = input("What is your name?  ")
 age = input("What is your age?  ")
 #number = int(input("Please enter a random number.  "))
